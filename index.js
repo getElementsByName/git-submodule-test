5df6e3d225e67f11eb5e902f7eb7d819b4dc6ca0
wh.y <wh.y@navercorp.com>
2019-09-26 07:19:39 +0900

    update

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-const SubModule = require('sub-package-test')
+const SubPackageFromPackageJson = require('sub-package-test')
+const SubPackageFromSubModule = require('./submodule-target')
+
+console.log('SubPackageFromPackageJson', SubPackageFromPackageJson)
+console.log('SubPackageFromSubModule', SubPackageFromSubModule)
